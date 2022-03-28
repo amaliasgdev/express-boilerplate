eeddb416e5162f2b2a2cf3ede38112a3a23e295c
Amalia Sanchez <amaliasgdev@gmail.com>
2022-03-28 10:41:22 +0200

    Initial commit

---
+++ bin/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = require('../app');
+require('dotenv').config();
+
+const server = http.createServer(app);
+
+let PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor escuchando al puerto ${PORT}`);
+});
